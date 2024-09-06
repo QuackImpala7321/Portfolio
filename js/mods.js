2e1146e3dc60642263caa09615c9ef001203bcdd
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-09-05 21:14:27 -0500

    styled buttons

--- js/mods.js
+++ js/mods.js
@@ -1,75 +1,73 @@
 const modsDir = "/database/projects/mods"
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     const container = document.querySelector('.item-container')
-    getModList()
-    .then(mods => {
-        for (const dir of mods)
-            displayMod(dir, container)
-    })
-    .catch(e => console.error(e))
+    const mods = await getModList()
+    for (const dir of mods)
+        await displayMod(dir, container)
 })
 
 /**
  * Adds the mod page to 
  * @param {string} dir 
  * @param {Element} container
+ * @returns {Promise<void>}
  */
-function displayMod(dir, container) {
+async function displayMod(dir, container) {
     const modDir = `${modsDir}/${dir}`
-    getJson(`${modDir}/manifest.json`)
-    .then(info => {
-        console.log(info)
+    const info = await getJson(`${modDir}/manifest.json`)
+    console.log(info)
 
-        const listNode = document.createElement('li')
-        const a = document.createElement('a')
-        a.className = 'mod-item'
-        a.href = info.source ? info.source : '#'
+    const listNode = document.createElement('li')
+    const buttons = document.createElement('div')
+    buttons.className = 'button-row'
+    const tile = document.createElement('div')
+    tile.className = 'mod-item'
+    const body = document.createElement('div')
+    body.className = 'mod-item-body'
 
-        const image = document.createElement('img')
-        const title = document.createElement('h2')
-        const paragraph = document.createElement('p')
-        const bottomRow = document.createElement('ol')
-        bottomRow.className = 'item-button-row'
+    const image = document.createElement('img')
+    const title = document.createElement('h2')
+    const paragraph = document.createElement('p')
 
-        // bottom row
-        const source = document.createElement('a')
-        const downloads = document.createElement('a')
-        source.className = 'source-buttom'
-        downloads.className = 'source-button'
-        const sourceParagraph = document.createElement('p')
-        const downloadsParagraph = document.createElement('p')
+    if (info.source) {
+        const button = document.createElement('a')
+        const p = document.createElement('p')
+        p.appendChild(document.createTextNode('Source'))
+        button.href = info.source
+        button.appendChild(p)
+        buttons.appendChild(button)
+    }
+    if (info.downloads) {
+        const button = document.createElement('a')
+        const p = document.createElement('p')
+        p.appendChild(document.createTextNode('Downloads'))
+        button.href = info.downloads
+        button.appendChild(p)
+        buttons.appendChild(button)
+    }
 
-        sourceParagraph.appendChild(document.createTextNode('Source'))
-        downloadsParagraph.appendChild(document.createTextNode('Downloads'))
-        source.appendChild(sourceParagraph)
-        downloads.appendChild(downloadsParagraph)
-        bottomRow.appendChild(source)
-        bottomRow.appendChild(downloads)
+    // item body
+    image.src = `${modDir}/icon.png`
+    title.appendChild(document.createTextNode(info.name))
+    paragraph.appendChild(document.createTextNode(info.short_description))
 
-        // item body
-        image.src = `${modDir}/icon.png`
-        title.appendChild(document.createTextNode(info.name))
-        paragraph.appendChild(document.createTextNode(info.short_description))
-
-        listNode.appendChild(a)
-        a.appendChild(image)
-        a.appendChild(title)
-        a.appendChild(paragraph)
-        a.appendChild(bottomRow)
-        container.appendChild(a)
-    })
-    .catch(e => console.error(e))
+    tile.appendChild(body)
+    tile.appendChild(buttons)
+    listNode.appendChild(tile)
+    body.appendChild(image)
+    body.appendChild(title)
+    body.appendChild(paragraph)
+    container.appendChild(listNode)
 }
 
 /**
  * Gets the mod list from mods.json
  * @returns {Promise<string[]>}
  */
-function getModList() {
-    return getJson(`${modsDir}/mods.json`)
-    .then(json => json.dirs)
-    .catch(e => console.error(e))
+async function getModList() {
+    const json = await getJson(`${modsDir}/mods.json`)
+    return json.dirs
 }
 
 /**
@@ -77,8 +75,7 @@ function getModList() {
  * @param {string} path 
  * @returns {Promise<any>}
  */
-function getJson(path) {
-    return fetch(path)
-    .then(res => res.json())
-    .catch(e => console.error(e))
+async function getJson(path) {
+    const contents = await fetch(path)
+    return await contents.json()
 }
