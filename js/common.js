635ce71bea21e82be6afd6d0d5ed97406c8b9339
QuackImpala7321 <rm3701645@gmail.com>
2024-09-06 08:40:11 -0500

    fixed header position

--- js/common.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    header = document.querySelector('.navbar')
-    window.onscroll = fixHeaderPosition
-})
-
-function fixHeaderPosition(e) {
-    console.log(window.scrollX)
-}
