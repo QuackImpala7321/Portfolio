b5919e789911fedf6c01bfbabb046558fb238884
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-09-18 23:56:31 -0500

    finished up styling for newsletter

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+    props: ['text']
+}
+</script>
+
+<template>
+    <div class="checkbox">
+        <input type="checkbox">
+        <p v-if="text">{{ text }}</p>
+    </div>
+</template>
--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,41 +1,61 @@
 <script lang="ts">
-import { API_URL } from '@/pages/Home/index.ts';
+import { API_URL } from '@/pages/Home/index.ts'
+import Checkbox from './Checkbox.vue'
+
+async function submit() {
+	const confirmButton = document.querySelector('.confirm-button') as HTMLElement
+	const enabled = confirmButton.getAttribute('enabled') === 'true'
+	if (!enabled)
+		return
+
+	const emailElement: any = document.getElementById('email')
+	const email: string = emailElement.value
+
+	if (!email.includes('@') || !email.includes('.'))
+		return
+
+	const text = confirmButton.firstElementChild as HTMLElement
+	const sourceBox: any = document.getElementById('source-upd')?.firstElementChild
+
+	sourceBox.disabled = true
+	text.innerText = "Submitted!"
+	emailElement.setAttribute('readonly', 'readonly')
+	confirmButton.setAttribute('enabled', 'false')
+	
+	await fetch(API_URL, {
+		method: "POST",
+		mode: "cors",
+		headers: {
+			"Content-Type": "text/plain"
+		},
+		body: JSON.stringify({
+			email: email,
+			source: sourceBox.checked
+		})
+	})
+}
 
 export default {
-	methods: {
-		async submit(e: MouseEvent) {
-			const emailElement: any = document.getElementById('email')
-			const email: string = emailElement.value
-
-			if (!email.includes('@') || !email.includes('.'))
-				return
-
-			console.log(email)
-			const res = await fetch(API_URL, {
-				method: "POST",
-				mode: "cors",
-				headers: {
-					"Content-Type": "text/plain"
-				},
-				body: email
-			})
-
-			const form: any = (e.target as Element).closest(".newsletter-form")
-			form.style.display = 'none'
-		}
+	mounted() {
+		const button = document.querySelector('.confirm-button') as HTMLElement
+		button.addEventListener('click', submit)
+	},
+	components: {
+		Checkbox
 	}
 }
 </script>
 
 <template>
 	<form class="newsletter-form">
-		<h1>Sign up for the Newsletter!</h1><br>
-		<label for="email">Email</label>
-		<input class="news-input" id="email" type="text" placeholder="example@example.com">
-		<div class="button-row">
-			<a href="#" @click="submit">
-				<p>Submit</p>
-			</a>
+		<div class="news-body">
+			<h1>Sign up for the Newsletter!</h1>
+			<h2>Email</h2>
+			<input class="news-input" id="email" type="text" placeholder="example@example.com">
+			<Checkbox id="source-upd" text="Subscribe to source updates" />
+		</div>
+		<div class="confirm-button" enabled="true">
+			<p>Confirm</p>
 		</div>
 	</form>
 </template>
