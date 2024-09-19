94e516a2c33c8650b31621dc617d55ee12225a82
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-09-19 17:08:14 -0500

    fixed modsDir path

--- src/pages/Mods/mods.ts
+++ src/pages/Mods/mods.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './Mods.vue'
 
-export const modsDir = "/database/projects/"
+export const modsDir = "/database/projects"
 createApp(App).mount('#app')
