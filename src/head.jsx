80840bce94ac2aca1c7d70150cdaf9b26bebbd37
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-09-20 15:49:02 -0500

    experimenting with jsx for headers

---
+++ src/head.jsx
@@ -0,0 +1,7 @@
+<>
+    <meta name="theme-color" content="#ffffff" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Quack's Portfolio" />
+    <meta property="og:description" content="Showcases my mods" />
+    <meta property="og:url" content="https://quackimpala7321.github.io" />
+</>
