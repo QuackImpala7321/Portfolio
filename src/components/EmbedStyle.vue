7e51533758b45564bea760cbce32a48932f7700f
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-09-20 18:15:59 -0500

    didn't work out :/

---
+++ src/components/EmbedStyle.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+    props: ['desc'],
+    setup(props, ctx) {
+        const title = ref(document.title)
+        const url = ref(document.URL)
+
+        return { title, url }
+    }
+}
+</script>
+
+<template>
+<head>
+    <meta name="theme-color" content="#ffffff" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" :content="title" />
+    <meta property="og:description" :content="desc" v-if="desc" />
+    <meta property="og:url" :content="url" />
+</head>
+</template>
